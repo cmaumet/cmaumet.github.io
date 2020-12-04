66b899ab208b363e52c6ec5cd2eb517709538b2a
cmaumet <camille.maumet@inria.fr>
2020-12-04 15:11:31 +0100

    no et al in publication list

--- scripts/update_publis.py
+++ scripts/update_publis.py
@@ -5,7 +5,7 @@ import os
 script_path = os.path.dirname(os.path.abspath(__file__))
 
 # Read publication list from hal
-hal_url = 'http://haltools.archives-ouvertes.fr/Public/afficheRequetePubli.php?auteur_exp=camille,maumet&CB_ref_biblio=oui&langue=Anglais&tri_exp=annee_publi&tri_exp2=typdoc&tri_exp3=date_publi&ordre_aff=TA&Fen=Aff&typdoc=(%27ART%27,%27COMM%27,%27PRESCONF%27,%27OUV%27,%27COUV%27,%27DOUV%27,%27PATENT%27,%27OTHER%27,%27UNDEFINED%27,%27REPORT%27,%27THESE%27,%27HDR%27,%27MEM%27,%27LECTURE%27,%27IMG%27,%27VIDEO%27,%27SON%27,%27MAP%27,%27MINUTES%27,%27NOTE%27,%27OTHERREPORT%27,%27SYNTHESE%27)&CB_DOI=oui&invitedCommunication=Non&popularLevel=Non'
+hal_url = 'http://haltools.archives-ouvertes.fr/Public/afficheRequetePubli.php?auteur_exp=camille,maumet&CB_auteur=oui&CB_titre=oui&CB_article=oui&langue=Anglais&tri_exp=annee_publi&tri_exp2=typdoc&tri_exp3=date_publi&ordre_aff=TA&Fen=Aff&typdoc=(%27ART%27,%27COMM%27,%27PRESCONF%27,%27OUV%27,%27COUV%27,%27DOUV%27,%27PATENT%27,%27OTHER%27,%27UNDEFINED%27,%27REPORT%27,%27THESE%27,%27HDR%27,%27MEM%27,%27LECTURE%27,%27IMG%27,%27VIDEO%27,%27SON%27,%27MAP%27,%27MINUTES%27,%27NOTE%27,%27OTHERREPORT%27,%27SYNTHESE%27)&CB_DOI=oui&invitedCommunication=Non&popularLevel=Non'
 
 response = urlopen(hal_url)
 html = response.read().decode("utf-8") 
@@ -19,43 +19,7 @@ found = re.search(r'<body>(.*)</body>', html, re.DOTALL)
 publis = found.group(0).replace("<body>", "").replace("</body>", "")
 
 replacements = (
-    ("Krzysztof Gorgolewski, Gael Varoquaux, Gabriel Rivera\
-, Yannick Schwarz, Satrajit Ghosh, et al.", "Krzysztof Gorgolewski, Gael \
-Varoquaux, Gabriel Rivera, Yannick Schwartz, Satrajit Ghosh, Camille Maumet, \
-Vanessa Sochat, Thomas Nichols, Russell Poldrack, Jean-Baptiste Poline, Tal \
-Yarkoni, Daniel Margulies"),
-    ("Krzysztof Gorgolewski, Gael Varoquaux, Gabriel Rivera\
-, Yannick Schwartz, Vanessa Sochat, et al.", "Krzysztof Gorgolewski, Gael \
-Varoquaux, Gabriel Rivera, Yannick Schwartz, Vanessa Sochat, Satrajit Ghosh\
-, Camille Maumet, Thomas Nichols, Jean-Baptiste Poline, Tal Yarkoni, Daniel \
-Margulies, Russell Poldrack"),
-    ("Krzysztof Gorgolewski, Tal Yarkoni, Satrajit Ghosh, \
-Russel Poldrack, Jean-Baptiste Poline, et al.", "Krzysztof Gorgolewski, \
-Tal Yarkoni, Satrajit Ghosh, Russel Poldrack, Jean-Baptiste Poline, Yannick \
-Schwartz, Thomas Nichols, Camille Maumet, Daniel Margulies"),
-    ("Ruth Pauli, Alexander Bowring, Richard Reynolds, Gang\
- Chen, Thomas Nichols, et al.", "Ruth Pauli, Alexander Bowring, Richard \
-Reynolds, Gang Chen, Thomas Nichols, Camille Maumet"),
-    ("Krzysztof Gorgolewski, Tibor Auer, Vince Calhoun,\
- Cameron Craddock, Samir Das, et al.", "Krzysztof Gorgolewski, Tibor Auer, \
-Vince Calhoun,\
- Cameron Craddock, Samir Das, Eugene Duff, Guillaume Flandin, Satrajit Ghosh,\
- Tristan Glatard, Yaroslav Halchenko, Daniel Handwerker, Michael Hanke,\
- David Keator, Xiangrui Li, Zachary Michael, Camille Maumet, Nolan Nichols, \
- Thomas Nichols, John Pellman, Jean-Baptiste Poline, Ariel Rokem,\
- Gunnar Schaefer, Vanessa Sochat, William Triplett, Jessica Turner,\
- Gael Varoquaux, Russell Poldrack"),
-    ("Ruth Pauli, Alexander Bowring, Richard Reynolds, \
-Gang Chen, Thomas E. Nichols, et al.", "Ruth Pauli, Alexander Bowring, Richard\
- Reynolds, Gang Chen, Thomas E. Nichols, Camille Maumet"),
-    ("Camille Maumet, Tibor Auer, Alexander Bowring, Gang \
-Chen, Samir Das, et al.", "Camille Maumet, Tibor Auer, Alexander Bowring, Gang\
- Chen, Samir Das, Guillaume Flandin, Satrajit Ghosh, Tristan Glatard, \
-Krzysztof J. Gorgolewski, Karl G. Helmer, Mark Jenkinson, David B. Keator, B. \
-Nolan Nichols, Jean-Baptiste Poline, Richard Reynolds, Vanessa Sochat, \
-Jessica Turner, Thomas E. Nichols"),
-    ("Frontiers in Aging Neuroscience", "Frontiers in Neuroscience"),
-    ("Camille Maumet", "<b>Camille Maumet</b>")
+    ("Camille Maumet", "<b>Camille Maumet</b>"),
 )
 
 for to_rep, rep in replacements:
@@ -65,7 +29,7 @@ with open(os.path.join(script_path, '..', 'publications.html'), 'wb') as f:
     f.write((head+publis+bottom).encode('ascii', 'xmlcharrefreplace'))
 
 # Two last publications
-response = urlopen('http://haltools.archives-ouvertes.fr/Public/afficheRequetePubli.php?auteur_exp=camille,maumet&NbAffiche=3&CB_ref_biblio=oui&langue=Anglais&tri_exp=annee_publi&tri_exp2=date_publi&ordre_aff=TA&Fen=Aff&invitedCommunication=Non&popularLevel=Non')
+response = urlopen('https://haltools.archives-ouvertes.fr/Public/afficheRequetePubli.php?auteur_exp=camille,maumet&NbAffiche=3&CB_ref_biblio=oui&langue=Anglais&tri_exp=date_publi&ordre_aff=TA&Fen=Aff&invitedCommunication=Non&popularLevel=Non')
 
 html = response.read().decode("utf-8") 
 with open(os.path.join(script_path, 'include/index_head.html'), 'r') as f:
@@ -75,6 +39,54 @@ with open(os.path.join(script_path, 'include/index_foot.html'), 'r') as f:
 found = re.search(r'<body>(.*)</body>', html, re.DOTALL)
 
 publis = found.group(0).replace("<body>", "").replace("</body>", "")
+
+replacements = (
+    ("Camille Maumet", "<b>Camille Maumet</b>"),
+)
+
+# replacements = (
+#     ("Krzysztof Gorgolewski, Gael Varoquaux, Gabriel Rivera\
+# , Yannick Schwarz, Satrajit Ghosh, et al.", "Krzysztof Gorgolewski, Gael \
+# Varoquaux, Gabriel Rivera, Yannick Schwartz, Satrajit Ghosh, Camille Maumet, \
+# Vanessa Sochat, Thomas Nichols, Russell Poldrack, Jean-Baptiste Poline, Tal \
+# Yarkoni, Daniel Margulies"),
+#     ("Krzysztof Gorgolewski, Gael Varoquaux, Gabriel Rivera\
+# , Yannick Schwartz, Vanessa Sochat, et al.", "Krzysztof Gorgolewski, Gael \
+# Varoquaux, Gabriel Rivera, Yannick Schwartz, Vanessa Sochat, Satrajit Ghosh\
+# , Camille Maumet, Thomas Nichols, Jean-Baptiste Poline, Tal Yarkoni, Daniel \
+# Margulies, Russell Poldrack"),
+#     ("Krzysztof Gorgolewski, Tal Yarkoni, Satrajit Ghosh, \
+# Russel Poldrack, Jean-Baptiste Poline, et al.", "Krzysztof Gorgolewski, \
+# Tal Yarkoni, Satrajit Ghosh, Russel Poldrack, Jean-Baptiste Poline, Yannick \
+# Schwartz, Thomas Nichols, Camille Maumet, Daniel Margulies"),
+#     ("Ruth Pauli, Alexander Bowring, Richard Reynolds, Gang\
+#  Chen, Thomas Nichols, et al.", "Ruth Pauli, Alexander Bowring, Richard \
+# Reynolds, Gang Chen, Thomas Nichols, Camille Maumet"),
+#     ("Krzysztof Gorgolewski, Tibor Auer, Vince Calhoun,\
+#  Cameron Craddock, Samir Das, et al.", "Krzysztof Gorgolewski, Tibor Auer, \
+# Vince Calhoun,\
+#  Cameron Craddock, Samir Das, Eugene Duff, Guillaume Flandin, Satrajit Ghosh,\
+#  Tristan Glatard, Yaroslav Halchenko, Daniel Handwerker, Michael Hanke,\
+#  David Keator, Xiangrui Li, Zachary Michael, Camille Maumet, Nolan Nichols, \
+#  Thomas Nichols, John Pellman, Jean-Baptiste Poline, Ariel Rokem,\
+#  Gunnar Schaefer, Vanessa Sochat, William Triplett, Jessica Turner,\
+#  Gael Varoquaux, Russell Poldrack"),
+#     ("Ruth Pauli, Alexander Bowring, Richard Reynolds, \
+# Gang Chen, Thomas E. Nichols, et al.", "Ruth Pauli, Alexander Bowring, Richard\
+#  Reynolds, Gang Chen, Thomas E. Nichols, Camille Maumet"),
+#     ("Camille Maumet, Tibor Auer, Alexander Bowring, Gang \
+# Chen, Samir Das, et al.", "Camille Maumet, Tibor Auer, Alexander Bowring, Gang\
+#  Chen, Samir Das, Guillaume Flandin, Satrajit Ghosh, Tristan Glatard, \
+# Krzysztof J. Gorgolewski, Karl G. Helmer, Mark Jenkinson, David B. Keator, B. \
+# Nolan Nichols, Jean-Baptiste Poline, Richard Reynolds, Vanessa Sochat, \
+# Jessica Turner, Thomas E. Nichols"),
+#     ("Frontiers in Aging Neuroscience", "Frontiers in Neuroscience"),
+#     ("Patricia Clement, Thomas Booth, Fran Borovečki, Kyrre Emblem, Patrícia Figueiredo, et al.", 
+#         "Patricia Clement, Thomas Booth, Fran Borovečki, Kyrre Emblem, Patrícia Figueiredo, \
+# Lydiane Hirschler, Radim Jančálek, Vera C. Keil, Camille Maumet, et al."),
+#     ("Camille Maumet", "<b>Camille Maumet</b>")
+# )
+
 
 for to_rep, rep in replacements:
     publis = publis.replace(to_rep, rep)
